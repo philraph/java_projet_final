6c6f24cafcd46e8b4fd8b97acc6a8d9c26bde555
Alan-denis <alan.denis@efrei.net>
2023-10-12 10:12:30 +0200

    Deplacement des controleurs

--- src/main/java/net/efrei/java_projet_final/servlet/LoginServlet.java
+++ src/main/java/net/efrei/java_projet_final/servlet/LoginServlet.java
@@ -1,13 +1,33 @@
 package net.efrei.java_projet_final.servlet;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
+import net.efrei.java_projet_final.services.UtilisateurService;
 
 import java.io.IOException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    private UtilisateurService _userService = new UtilisateurService();
+
+    boolean login(String username, String mdp){
+
+        var user = _userService.findByUsername(username);
+
+        // Si l'utilisateur existe
+        if ( user != null ) {
+            BCrypt.Result result = BCrypt.verifyer().verify(mdp.toCharArray(), user.getPassword());
+
+            return result.verified;
+        }
+        else {
+            // L'utilisateur n'existe pas
+            return false;
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/java/net/efrei/java_projet_final/servlet/RegisterServlet.java
+++ src/main/java/net/efrei/java_projet_final/servlet/RegisterServlet.java
@@ -1,16 +1,39 @@
 package net.efrei.java_projet_final.servlet;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.efrei.java_projet_final.services.UtilisateurService;
 
 import java.io.IOException;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
+
+    private UtilisateurService _userService = new UtilisateurService();
+
+    boolean register(String username, String mdp){
+
+        var user = _userService.findByUsername(username);
+
+        if(user != null){
+            // Le nom d'utilisateur existe déjà
+            return false;
+        }
+        else {
+            String bcryptHashString = BCrypt.withDefaults().hashToString(12, mdp.toCharArray());
+            user.setPassword(bcryptHashString);
+            user.setUsername(username);
+
+            _userService.register(user);
+            return false;
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher("/register.jsp");
