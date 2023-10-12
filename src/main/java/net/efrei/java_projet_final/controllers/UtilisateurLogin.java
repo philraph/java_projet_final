455e3ef82aeb50595820e7da98c19338fc1d594d
Alan-denis <alan.denis@efrei.net>
2023-10-12 10:07:43 +0200

    controleurs terminer

--- src/main/java/net/efrei/java_projet_final/controllers/UtilisateurLogin.java
+++ src/main/java/net/efrei/java_projet_final/controllers/UtilisateurLogin.java
@@ -1,6 +1,7 @@
 package net.efrei.java_projet_final.controllers;
 
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import net.efrei.java_projet_final.services.UtilisateurService;
 
 public class UtilisateurLogin {
@@ -8,25 +9,18 @@ public class UtilisateurLogin {
     private UtilisateurService _userService = new UtilisateurService();
 
     boolean login(String username, String mdp){
-/*
+
         var user = _userService.findByUsername(username);
 
         // Si l'utilisateur existe
-        if ( user != null) {
+        if ( user != null ) {
             BCrypt.Result result = BCrypt.verifyer().verify(mdp.toCharArray(), user.getPassword());
 
-            if (result) {
-                return true;
-            }
+            return result.verified;
         }
-        else{
+        else {
             // L'utilisateur n'existe pas
-            return  false;
+            return false;
         }
-
-        return false;
-
- */
-        return false;
     }
 }
--- src/main/java/net/efrei/java_projet_final/controllers/UtilisateurRegister.java
+++ src/main/java/net/efrei/java_projet_final/controllers/UtilisateurRegister.java
@@ -1,9 +1,10 @@
 package net.efrei.java_projet_final.controllers;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import net.efrei.java_projet_final.services.UtilisateurService;
 
 public class UtilisateurRegister {
-/*
+
     private UtilisateurService _userService = new UtilisateurService();
 
     boolean register(String username, String mdp){
@@ -16,15 +17,14 @@ public class UtilisateurRegister {
         }
         else {
             String bcryptHashString = BCrypt.withDefaults().hashToString(12, mdp.toCharArray());
-            user.password = bcryptHashString;
-            user.username = username;
+            user.setPassword(bcryptHashString);
+            user.setUsername(username);
 
             _userService.register(user);
             return false;
         }
 
     }
- */
 }
 
 
