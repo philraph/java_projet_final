0ac1fc45e45a7eec376ee6a7d5c128371ce99e52
Alan-denis <alan.denis@efrei.net>
2023-11-10 22:05:43 +0100

    Rien

--- src/main/java/net/efrei/java_projet_final/servlet/UtilisateurServlet.java
+++ src/main/java/net/efrei/java_projet_final/servlet/UtilisateurServlet.java
@@ -4,30 +4,24 @@ import jakarta.inject.Inject;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.efrei.java_projet_final.entities.Utilisateur;
 import net.efrei.java_projet_final.services.UtilisateurService;
-import java.io.IOException;
 
+import java.io.IOException;
 import java.util.List;
 
 
 @WebServlet("/users")
-public class UtilisateurServlet extends HttpServlet {
+public class UtilisateurServlet {
 
     @Inject
     private UtilisateurService userService;
 
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         List<Utilisateur> users = userService.findByIsValid(true);
-
-        // Set the 'users' attribute in the request
-        req.setAttribute("validUsers", users);
-
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/pages/utilisateurValides.jsp");
         dispatcher.forward(req, resp);
     }
--- src/main/webapp/WEB-INF/pages/valideUsers.jsp
+++ src/main/webapp/WEB-INF/pages/valideUsers.jsp
@@ -5,33 +5,16 @@
   Time: 21:38
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page import="java.util.List" %>
-<%@ page import="net.efrei.java_projet_final.entities.Utilisateur" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<!DOCTYPE html>
-<html lang="fr">
+<%@ page import="net.efrei.java_projet_final.entities.Utilisateur" %>
+<html>
 <head>
-    <title>Utilisateur valides</title>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <title>Utilisateurs valides</title>
+    <link rel='stylesheet' href='https://picocss.com/docs/css/pico.docs.min.css'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-    <link rel="stylesheet" href="/resources/css/styles.css"/>
 </head>
+
 <body>
-
-<h1>List of Valid Users</h1>
-
-<ul>
-    <c:forEach var="user" items="${validUsers}">
-        <div>
-            <h1>${user.username}</h1>
-            <p>${user.id}</p>
-        </div>
-    </c:forEach>
-</ul>
-
-</body>
-</html>
-
 
 
 </body>
