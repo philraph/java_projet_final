befdaa6dacf0188435d77e41a84ea0a520d3d38c
Alan Denis <88785284+Alan-denis@users.noreply.github.com>
2023-11-10 21:59:49 +0100

    Add files via upload
    Utilisateur servlet

---
+++ src/main/java/net/efrei/java_projet_final/servlet/UtilisateurServlet.java
@@ -0,0 +1,29 @@
+package net.efrei.java_projet_final.servlet;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.efrei.java_projet_final.entities.Utilisateur;
+import net.efrei.java_projet_final.services.UtilisateurService;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet("/users")
+public class UtilisateurServlet {
+
+    @Inject
+    private UtilisateurService userService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<Utilisateur> users = userService.findByIsValid(true);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/pages/utilisateurValides.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
